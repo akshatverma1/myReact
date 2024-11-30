7bd70a81e9552aae8b012636b72e08ef86f978ac
akshatverma1 <akshaverma14@gmail.com>
2024-11-30 16:08:48 +0530

    usecallback

--- src/component/useCallback.jsx
+++ src/component/useCallback.jsx
@@ -3,7 +3,7 @@ import React,{useCallback, useState} from 'react';
 export default function Usecallback(){
     let[data,setData]=useState();
 
-    //the callback hook in react memorizes a function ,ensuring ir does not get recreated unnecessarily on every render.
+    //the callback hook in react memorizes a function ,ensuring is does not get recreated unnecessarily on every render.
     const memory = useCallback(()=>{
         console.log(data);
     },[setData()])
