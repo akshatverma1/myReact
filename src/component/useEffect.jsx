22eb1edc8fc3d268cda8273d882dadc5cf09b976
akshatverma1 <akshaverma14@gmail.com>
2024-11-30 15:33:35 +0530

    useEffect

--- src/component/useEffect.jsx
+++ src/component/useEffect.jsx
@@ -1,25 +1,34 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Api(){
+export default function Api() {
     let Url = "https://dummyjson.com/products";
-    let [apidta,setApidta] = useState([]);
-    useEffect(()=>{
-        fetch(Url).then((Response)=>{Response.json().then((data)=>{setApidta(data.products)})});
-    },[])
+    let [apidta, setApidta] = useState([]);
+
+    let details =
+        "when the In dependices array is null then the useeffect is call when the page is reload.when the dependices section is null no array and no Dependencies the then useEffect is call every re-render";
+    console.log(details);
+
+
+    useEffect(() => {
+        fetch(Url).then((Response) => { Response.json().then((data) => { setApidta(data.products) }) });
+    }, [])
     console.log(apidta);
+
+
+
     return (
         <>
-        <h1>Api Fetch</h1>
-        {apidta.map((item,index)=>{
-            return (
-                <>
-                <p style={{display:"inline",fontWeight:700}}> - {item.id}</p>
-                <p style={{display:"inline",fontWeight:700}}> {item.title}</p>
-                <br></br>
-                <br></br>
-                </>
-            )
-        })}
+            <h1>Api Fetch</h1>
+            {apidta.map((item, index) => {
+                return (
+                    <>
+                        <p style={{ display: "inline", fontWeight: 700 }}> - {item.id}</p>
+                        <p style={{ display: "inline", fontWeight: 700 }}> {item.title}</p>
+                        <br></br>
+                        <br></br>
+                    </>
+                )
+            })}
         </>
     )
 }
