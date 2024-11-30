0f2140516168b282aed9e014a45682ad64328650
akshatverma1 <akshaverma14@gmail.com>
2024-11-30 16:08:07 +0530

    usecallback

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Usercontext } from './component/context';
-import Display from "./component/Display";
 import { useState } from 'react';
-import Item from './component/Item';
 import { Route,BrowserRouter,Routes } from 'react-router-dom';
 function App() {
   let [datas,setdatas] = useState("akshat");
   return (
     <>
-    <BrowserRouter>
-    <Usercontext.Provider value={datas}>
-      <Routes>
-        <Route path='/' element={<Display></Display>}></Route>
-        <Route path='/item' element={<Item></Item>}></Route>
-      </Routes>
-    {/* <h1>Context</h1> */}
-    </Usercontext.Provider>
-    </BrowserRouter>
     </>
   );
 }
---
+++ src/component/useCallback.jsx
@@ -0,0 +1,16 @@
+import React,{useCallback, useState} from 'react';
+
+export default function Usecallback(){
+    let[data,setData]=useState();
+
+    //the callback hook in react memorizes a function ,ensuring ir does not get recreated unnecessarily on every render.
+    const memory = useCallback(()=>{
+        console.log(data);
+    },[setData()])
+
+
+    return (
+        <>
+        </>
+    )
+}
