c619b171dcee4c8927517ff22701093df1b3abb2
akshatverma1 <akshaverma14@gmail.com>
2024-11-30 15:34:57 +0530

    useEffect

--- src/component/useEffect.jsx
+++ src/component/useEffect.jsx
@@ -4,10 +4,6 @@ export default function Api() {
     let Url = "https://dummyjson.com/products";
     let [apidta, setApidta] = useState([]);
 
-    let details =
-        "when the In dependices array is null then the useeffect is call when the page is reload.when the dependices section is null no array and no Dependencies the then useEffect is call every re-render";
-    console.log(details);
-
 
     useEffect(() => {
         fetch(Url).then((Response) => { Response.json().then((data) => { setApidta(data.products) }) });
